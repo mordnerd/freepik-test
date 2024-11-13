7cc5bf71dfb2db3aa176fbfd0004bdee59d42e42
Dada Mordecai <dadamd@Dadas-MacBook-Pro.local>
2024-11-12 20:43:09 -0500

    freepik homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
+import { Main } from "next/document";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <div className="bg-[url('/pexels-photo-1685413.webp')] pb-8">
+        <div className="flex justify-between pl-4 gap-8 space-y-1.5 pt-4 text-white">
+          <ol className="flex gap-4 space-y-1.5 items-center">
+            <li className="text-2xl font-bold p-1"><button>FREEPIK</button></li>
+            <li><button>Tools</button></li>
+            <li><button>Images</button></li>
+            <li><button>Icons</button></li>
+            <li><button>Videos</button></li>
+            <li><button>Templates</button></li>
+            <li><button>PSD</button></li>
+            <li><button>Mockups</button></li>
+            <li><button>More</button></li>
+          </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <ol className="flex pr-6 gap-6 items-center">
+            <li><button>Pricing</button></li>
+            <li><button className="border px-3 py-1 rounded">Sign-in</button></li>
+          </ol>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <main>
+            <h2 className="flex text-3xl font-bold text-4xl justify-center pb-6 pt-16 text-white">Creating great designs, faster</h2>
+            <p className="flex justify-center text-xl text-white">High quality photos, video, vectors, PSD, AI images, icons... to go from ideas to outstanding designs</p>
+          </main>
+        </div>
+
+        <div className="flex justify-center my-10 bg-white p-2 w-3/5 m-auto h-16 rounded">
+          <div className="px-4 flex border-r">
+            <select className="text-lg pr-20">
+              <option>Asset</option>
+              <option>PSD</option>
+            </select>
+          </div>
+          <input className="flex-1 rounded px-4" placeholder="Search all assets"/>
+          <button className="text-white bg-blue-700 rounded px-8 font-semibold">Search</button>
+        </div>
+
+        <div className="flex justify-center gap-3 text-sm text-white pb-20">
+          <button className="bg-gray-400/25 p-2 rounded">happy birthday</button>
+          <button className="bg-gray-400/25 p-2 rounded">christmas tree</button>
+          <button className="bg-gray-400/25 p-2 rounded">iphone mockup</button>
+        </div>
+      </div>
+
+
+      <div className="flex gap-8 bg-black h-24 justify-center items-center" >
+        <p className="text-white text-2xl text-yellow-500 font-bold">40% off premium plans</p>
+        <div className="flex gap-1 items-center">
+          <p className="text-white font-bold text-xl">only</p>
+          <div>
+            <div className="flex bg-white rounded text-black px-3 py-1 font-semibold text-base">08:45:02</div>
+          </div>
+          <p className="text-white font-bold text-xl">left!</p>
+        </div>
+      </div>
+
+      <div>
+        <h1 className="font-bold justify-center pl-16 pt-10 text-3xl">Free tools to skyrocket your creative freedom</h1>
+      </div>
+
+      <div className="flex gap-8 pt-10 w-full rounded-md">
+        <div>
+          <img src="https://fps.cdnpk.net/home/banner-tools/image-generator-bg.webp" />
+        </div>
+        <div>
+          <img src="https://fps.cdnpk.net/home/banner-tools/designer-bg.webp" />
+        </div>
+        <div>
+          <img src="https://fps.cdnpk.net/home/banner-tools/mockup-bg.webp" />
+        </div>
+
+        <div>
+          <img src="https://fps.cdnpk.net/home/banner-tools/reimagine-bg.webp" />
+        </div>
+        <div>
+          <img src="https://fps.cdnpk.net/home/banner-tools/retouch.webp" />
+        </div>
+      </div>
     </div>
   );
 }
